41b7d985b9ccf49d680217b7cc3c649a9728ecb1
Helder Alves Couto <helder@brassun.com.br>
2021-07-24 22:52:41 -0300

    elements count list

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,11 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: list_count_elem(list, 0)
+
+  defp list_count_elem([_head | tail], acc) do
+    acc = acc + 1
+    list_count_elem(tail, acc)
   end
+
+  defp list_count_elem([], acc), do: acc
+
 end
